247bfa2053d4f075a98098e939b3ab6a04c97f87
Gabil Gurbanov <57041974+ggurbanov12098@users.noreply.github.com>
2024-03-08 14:28:02 +0400

    Working Checkpoint
    Contains everything to run project, tests, wrapper and docker for execution

---
+++ src/test/java/ada/edu/demo/webtest/config/SeleniumConfig.java
@@ -0,0 +1,37 @@
+package ada.edu.demo.webtest.config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+// import org.springframework.stereotype.Component;
+
+@Configuration
+public class SeleniumConfig {
+
+    @Bean
+    public WebDriver webDriver() {
+        // Configure Chrome to run in headless mode
+        // FirefoxOptions options = new FirefoxOptions();
+        // options.addArguments("--headless");
+
+        // Configure Chrome to run in headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments();
+
+
+
+        // WebDriverManager.firefoxdriver().setup();
+        // return new FirefoxDriver(options);
+        // return new FirefoxDriver();
+        
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(options);
+        // return new FirefoxDriver();
+
+    }
+}
