2a96205e2e77328cc13a8f50e524c7bf4e2e076c
Gabil Gurbanov <57041974+ggurbanov12098@users.noreply.github.com>
2024-03-12 18:12:07 +0400

    WebInterface test
    WebInterface test added
    Cleaned unnecessary comments and unused code in some files

--- src/test/java/ada/edu/demo/webtest/WebInterfaceTests.java
+++ src/test/java/ada/edu/demo/webtest/WebInterfaceTests.java
@@ -4,15 +4,10 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.support.ui.ExpectedConditions;
-// import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-
 import java.util.List;
-
-// import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -41,13 +36,13 @@ class WebInterfaceTests {
 
 		try {
 			studentIdInput.sendKeys("12098");
-			Thread.sleep(1000);
+			Thread.sleep(500);
 			firstNameInput.sendKeys("Gabil");
-			Thread.sleep(1000);
+			Thread.sleep(500);
 			lastNameInput.sendKeys("Gurbanov");
-			Thread.sleep(1000);
+			Thread.sleep(500);
 			emailInput.sendKeys("ggurbanov12098@ada.edu.az");
-			Thread.sleep(1000);
+			Thread.sleep(500);
 		}
 		catch (Exception ex) {
 			System.out.println(ex);
@@ -74,9 +69,66 @@ class WebInterfaceTests {
 			throw new RuntimeException(e);
 		}
 
-		// Check if the text "Jamal" is present in the page content
+		// Check if the text "Gabil" is present in the page content
 		assert(bodyElementFName.size() == 1);
 		assert(bodyElementLName.size() == 1);
 	}
 
+
+	@Test
+	@Order(3)
+	@DisplayName("set course to student")
+	public void ChangeNameSurname(){
+		webDriver.get("http://localhost:"+port+"/student/list");
+		webDriver.get("http://localhost:"+port+"/student/update?id=12098");
+		
+		WebElement firstNameInput = webDriver.findElement(By.id("firstName"));
+		WebElement lastNameInput = webDriver.findElement(By.id("lastName"));
+		
+		// Check if such a field exists
+		assertNotNull(firstNameInput);
+		assertNotNull(lastNameInput);
+		
+		firstNameInput = webDriver.findElement(By.id("firstName"));
+		firstNameInput.clear();
+
+		lastNameInput = webDriver.findElement(By.id("lastName"));
+		lastNameInput.clear();
+
+		
+		try {
+			firstNameInput.sendKeys("Farid");
+			Thread.sleep(1000);
+			lastNameInput.sendKeys("Mammadli");
+			Thread.sleep(1000);	
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		// Find and submit the form (assuming there's a submit button with a specific attribute)
+		WebElement submitButton = webDriver.findElement(By.id("submit"));
+		submitButton.click();
+	}
+
+	@Test
+	@Order(4)
+	@DisplayName("Check the created user")
+	public void CheckNameSurname() {
+		// Check if the student is added
+		webDriver.get("http://localhost:"+port+"/student/list");
+		List<WebElement> bodyElementFName = webDriver.findElements(By.xpath("//*[contains(text(), 'Farid')]"));
+		List<WebElement> bodyElementLName = webDriver.findElements(By.xpath("//*[contains(text(), 'Mammadli')]"));
+		System.out.println("Element result"+bodyElementLName);
+
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Check if the text "Farid" is present in the page content
+		assert(bodyElementFName.size() == 1);
+		assert(bodyElementLName.size() == 1);
+	}
 }
